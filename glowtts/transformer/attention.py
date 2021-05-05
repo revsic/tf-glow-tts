ad12bc3b25a418ace200247a1538ed36b983d957
revsic <revsic99@gmail.com>
2021-05-05 23:20:33 +0900

    transformer: Add encoder transformer

---
+++ glowtts/transformer/attention.py
@@ -0,0 +1,69 @@
+from typing import Tuple
+
+import numpy as np
+import tensorflow as tf
+
+
+class MultiHeadSelfAttn(tf.keras.Model):
+    """Multihead Scaled-Dotproduct Self-attention block.
+    """
+    def __init__(self, channels: int, heads: int, dropout: float):
+        """Initializer.
+        Args:
+            channels: output channels.
+            heads: number of the heads.
+            dropout: dropout rate.
+        """
+        super().__init__()
+        self.heads = heads
+        self.channels = channels // heads
+        self.proj_q = tf.keras.layers.Dense(channels)
+        self.proj_k = tf.keras.layers.Dense(channels)
+        self.proj_v = tf.keras.layers.Dense(channels)
+        
+        self.dropout = tf.keras.layers.Dropout(dropout)
+        self.proj_out = tf.keras.layers.Dense(channels)
+    
+    def call(self, inputs: tf.Tensor, mask: tf.Tensor) \
+            -> Tuple[tf.Tensor, tf.Tensor]:
+        """Self-attend the inputs.
+        Args:
+            inputs: [tf.float32; [B, T, C]], input sequence.
+            mask: [tf.float32; [B, T]], mask tensor.
+        Returns:
+            out: [tf.float32; [B, T, C]], attended sequence.
+            attn: [tf.float32, [B, K, T, T]], dot-product attention.
+        """
+        # B, T, C
+        bsize, timestep, _ = tf.shape(inputs)
+        # [B, T, K, Ck]
+        query = tf.reshape(
+            self.proj_q(inputs), [bsize, timestep, self.heads, -1])
+        key = tf.reshape(
+            self.proj_k(inputs), [bsize, timestep, self.heads, -1])
+        value = tf.reshape(
+            self.proj_v(inputs), [bsize, timestep, self.heads, -1])
+
+        # [B, K, T, T]
+        scores = tf.matmul(
+            tf.transpose(query, [0, 2, 1, 3]),  # [B, K, T, Ck]
+            tf.transpose(key, [0, 2, 3, 1]))    # [B, K, Ck, T]
+        # scaling
+        scores = scores / np.sqrt(self.channels)
+        # masking
+        scores = scores * mask[:, None, None] + \
+            (1 - mask[:, None, None]) * tf.float32.min
+
+        # [B, K, T, T]
+        attn = tf.nn.softmax(scores, axis=-1)
+        attn = self.dropout(attn)
+        # [B, K, T, Ck]
+        attend = tf.matmul(
+            attn,
+            tf.transpose(value, [0, 2, 1, 3]))  # [B, K, T, Ck]
+        # [B, T, C]
+        out = tf.reshape(
+            tf.transpose(attend, [0, 2, 1, 3]),
+            [bsize, timestep, -1])
+        # [B, T, Cout], [B, K, T, T]
+        return self.proj_out(out) * mask[..., None], attn
---
+++ glowtts/transformer/pe.py
@@ -0,0 +1,53 @@
+import numpy as np
+import tensorflow as tf
+
+
+class PositionalEncodings(tf.keras.Model):
+    """Sinusoidal positional encoding generator.
+    """
+    def __init__(self, channels: int, presize: int = 128):
+        """Initializer.
+        Args:
+            channels: size of the channels.
+            presize: initial pe cache size.
+        """
+        super().__init__()
+        self.channels = channels
+        self.size = presize
+        self.buffer = self.generate(presize)
+
+    def call(self, size: int) -> tf.Tensor:
+        """Return cached positional encodings.
+        Args:
+            size: length of the pe.
+        Returns:
+            [tf.float32; [T, C]], sinusoidal positional encodings.
+        """
+        if size <= self.size:
+            return self.buffer[:size]
+        # generate new cache
+        self.buffer = self.generate(size)
+        return self.buffer
+
+    def generate(self, size: int) -> tf.Tensor:
+        """Generate positional encodings.
+        Args:
+            size: length of the pe.
+        Returns:
+           [tf.float32; [T, C]], sinusoidal positional encodings.
+        """
+        # [tf.int32; [T]]
+        pos = tf.range(size)
+        # [tf.int32; [C//2]]
+        i = tf.range(0, self.channels, 2)
+        # [C//C], casting for float64
+        denom = tf.exp(-np.log(10000) * tf.cast(i / self.channels, tf.float32))
+        # [T, C//2]
+        context = tf.cast(pos, tf.float32)[:, None] * denom[None]
+        # [T, C//2, 1]
+        context = context[..., None]
+        # [T, C//2, 2]
+        pe = tf.concat([tf.sin(context), tf.cos(context)], axis=-1)
+        # [T, C]
+        pe = tf.reshape(pe, [size, self.channels])
+        return pe
